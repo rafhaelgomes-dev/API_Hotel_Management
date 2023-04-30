0a7040e73c28f24b864ec61a047f11ee644281a3
Rafhael Gomes <rafhaelgomes.dev@gmail.com>
2023-04-30 20:39:37 -0300

    Ajustes permições da classe App

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import * as express from 'express';
+import * as cors from 'cors';
+import routers from './routes/main.routes';
 
 type port = number | string;
 
 export default class App {
-  public app: express.Express;
+  private app: express.Express;
 
   constructor() {
     this.app = express();
     this.config();
   }
 
-  public config = (): void => {
+  private config = (): void => {
     this.app.use(express.json);
+    this.app.use(cors());
+    this.app.use(routers);
   }
 
   public start = (PORT: port): void => {
